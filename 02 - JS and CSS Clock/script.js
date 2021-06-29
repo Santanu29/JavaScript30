d20f2e266c07a9657af2ba3932e2c975372a272d
Santanu Biswas <69161246+Santanu29@users.noreply.github.com>
2021-06-29 22:37:14 +0530

    added the changes

--- 02 - JS and CSS Clock/script.js
+++ 02 - JS and CSS Clock/script.js
@@ -1,13 +1,21 @@
 const secondHand = document.querySelector('.second-hand');
-const minuteHand = document.querySelector('.minute-hand');
+const minuteHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
 
 function setDate() {
     const now = new Date();
+
     const seconds = now.getSeconds();
-    const secondDegrees = (seconds * 360) / 60;
+    const secondDegrees = ((seconds * 360) / 60) + 90;
     secondHand.style.transform = `rotate(${secondDegrees}deg)`;
 
+    const minutes = now.getMinutes();
+    const minuteDegrees = ((minutes * 360) / 60) + 90;
+    minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
+
+    const hours = now.getHours();
+    const hourDegrees = ((hours * 360) / 12) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);

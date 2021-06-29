8534940462712f590fd885fc45ff00f0613f46ff
Santanu Biswas <69161246+Santanu29@users.noreply.github.com>
2021-06-29 20:39:57 +0530

    added hands of clock

--- 02 - JS and CSS Clock/script.js
+++ 02 - JS and CSS Clock/script.js
@@ -1,7 +1,13 @@
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.minute-hand');
+const hourHand = document.querySelector('.hour-hand');
+
 function setDate() {
     const now = new Date();
     const seconds = now.getSeconds();
-    console.log(seconds);
+
+    const secondDegrees = (seconds * 360) / 60;
+    console.log(secondDegrees);
 }
 
 setInterval(setDate, 1000);

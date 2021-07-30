9617c5653a46737bc024896406117e483af64137
santanu29 <santanubiswas2905@gmail.com>
2021-07-30 18:52:15 +0530

    formatter setting corrected

---
+++ 05 - Flex Panel Gallery/script.js
@@ -0,0 +1,10 @@
+const panels = document.querySelectorAll("panel");
+
+function toggleOpen() {
+    this.classList.toggle("open");
+}
+
+panels.forEach(panel => panel.addEventListener('open', toggleOpen()));
+
+
+

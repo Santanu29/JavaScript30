78f554e069e0ab137058400a27a97d888ad8f2ab
santanu29 <santanubiswas2905@gmail.com>
2021-08-01 20:40:42 +0530

    find function

--- 07 - Array Cardio Day 2/script.js
+++ 07 - Array Cardio Day 2/script.js
@@ -29,10 +29,24 @@ const isAdult = people.some((person) => {
 });
 
 console.log(isAdult);
+
+const everyAdult = people.every((person) => {
+	new Date().getFullYear() - person.year > 19;
+});
+
+console.log(everyAdult);
+
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
 
+// const comment = comments.find(function (comment) {
+// 	if (comment.id === 823423) return true;
+// });
+
+const comment = comments.find((comment) => comment.id === 823423);
+
+console.log(comment);
 // Array.prototype.findIndex()
 // Find the comment with this ID
 // delete the comment with the ID of 823423
